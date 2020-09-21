c7774bd1b0e32d39184f7979ba2e593dda39d7e0
Nattida Wong <nattidac.wong@gmail.com>
2020-09-21 17:09:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
 
-programmer_hash[:dennis_ritchie][:language].select do |language|
+programmer_hash[:dennis_ritchie][:languages].each do |language|
 	puts "#{language.to_s}"
 	end
 end
